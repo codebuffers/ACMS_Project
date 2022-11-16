2026ef350afcbdcbe23679d234c51fb1d156c4fe
Allaberdiev09 <p22014094@student.newinti.edu.my>
2022-11-16 17:01:11 +0800

    Fully implemented visit request function (java adapter, functions and xml); Drawable XML files for the design of Edit Texts and Buttons; The parent adapter for Officer which getting Model;

--- app/src/main/java/com/example/acms/ActivityOfficerPending.java
+++ app/src/main/java/com/example/acms/ActivityOfficerPending.java
@@ -1,11 +1,48 @@
 package com.example.acms;
 
-
+import android.annotation.SuppressLint;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ActivityOfficerPending extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    MainAdapterOfficer mainAdapterOfficer;
 
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_officer_pending);
+
+        recyclerView = (RecyclerView)findViewById(R.id.rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<Model> options =
+                new FirebaseRecyclerOptions.Builder<Model>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Users")
+                                .child("Visitors").orderByChild("visitorstatus").startAt("Pending").endAt("Pending\uf8ff"), Model.class)
+                        .build();
+
+        mainAdapterOfficer = new MainAdapterOfficer(options);
+        recyclerView.setAdapter(mainAdapterOfficer);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mainAdapterOfficer.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mainAdapterOfficer.startListening();
+    }
 }
