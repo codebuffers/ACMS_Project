9f11d1753030d1f34f9697acd5b9e386c4e00a3b
BrianFrost005 <brianlzx.bl@gmail.com>
2022-11-16 15:39:29 +0800

    files for officer login such as adapter and xml files

--- app/src/main/java/com/example/acms/ActivityOfficerLogin.java
+++ app/src/main/java/com/example/acms/ActivityOfficerLogin.java
@@ -1,7 +1,101 @@
 package com.example.acms;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.acms.databinding.ActivityOfficerLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ActivityOfficerLogin extends AppCompatActivity {
+
+    ActivityOfficerLoginBinding binding;
+    ProgressBar progressBar;
+    DatabaseReference root;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityOfficerLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.progressBar.setVisibility(View.INVISIBLE);
+
+        binding.LoginBtnId.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String OfficerSupID = binding.OfficerSuperIDId.getText().toString();
+
+                if (!OfficerSupID.isEmpty()) {
+                    binding.progressBar.setVisibility(View.VISIBLE);
+                    loadAccount (OfficerSupID);
+
+                } else {
+                    binding.progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(ActivityOfficerLogin.this, "Please enter your Officer super ID!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //Backt To the MainActivity Page function
+        binding.BackToMainBtnId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ActivityOfficerLogin.this, MainActivity.class));
+                binding.progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+
+    }
+
+    private void loadAccount(String OfficerSupID) {
+
+        root = FirebaseDatabase.getInstance().getReference("Users").child("wStaff");
+        root.child(OfficerSupID).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    if (task.getResult().exists()) {
+                        root.child(OfficerSupID).child("staffRole").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                int staffRole = snapshot.getValue(Integer.class);
+                                if (staffRole == 2) {
+                                    Toast.makeText(ActivityOfficerLogin.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(getApplicationContext(), ActivityOfficer.class));
+                                    finish();
+                                } else {
+                                    binding.progressBar.setVisibility(View.INVISIBLE);
+                                    Toast.makeText(ActivityOfficerLogin.this, "Not a valid Officer Super ID!", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                binding.progressBar.setVisibility(View.INVISIBLE);
+                                Toast.makeText(ActivityOfficerLogin.this, "Failed confirmation!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    } else  {
+                        binding.progressBar.setVisibility(View.INVISIBLE);
+                        Toast.makeText(ActivityOfficerLogin.this, "Not a valid Officer Super ID!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                } else {
+                    binding.progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(ActivityOfficerLogin.this, "Failed to login as Officer!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/acms/MainAdapterOfficer.java
+++ app/src/main/java/com/example/acms/MainAdapterOfficer.java
@@ -1,6 +1,194 @@
 package com.example.acms;
 
 
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainAdapterOfficer extends FirebaseRecyclerAdapter<Model, MainAdapterOfficer.myViewHolder> {
+
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public MainAdapterOfficer(@NonNull FirebaseRecyclerOptions<Model> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull myViewHolder holder, @SuppressLint("RecyclerView") int position, @NonNull Model model) {
+        holder.name.setText(model.getVisitorname());
+        holder.ic.setText("IC: "+model.getVisitoric());
+        holder.phone.setText("Phone: "+model.getVisitorphone());
+        holder.date.setText("Visit Date: "+model.getVisitdate());
+        holder.time.setText("Visit Time: "+model.getVisittime());
+        holder.extime.setText("Visit Exit Time: "+model.getVisitexittime());
+        holder.status.setText("Status: "+model.getVisitorstatus());
+        holder.ticket.setText("Ticket: "+model.getVisitorticket());
+        holder.reason.setText(model.getVisitreason());
+        holder.sreason.setText(model.getStatusreason());
+
+        Glide.with(holder.img.getContext())
+                .load(model.getVisitorimage())
+                .placeholder(com.firebase.ui.storage.R.drawable.common_google_signin_btn_icon_dark)
+                .error(com.firebase.ui.storage.R.drawable.common_google_signin_btn_icon_dark_normal)
+                .into(holder.img);
+
+        holder.btnReject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.img.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.reject_popup))
+                        .setExpanded(true, 800)
+                        .create();
+
+                //dialogPlus.show();
+
+                View v = dialogPlus.getHolderView();
+
+                EditText sreason = v.findViewById(R.id.txtReason);
+                String statusR = "Rejected";
+
+                Button btnConfirmReject = v.findViewById(R.id.btnConfirmReject);
+
+                //sreason.setText(model.getStatusreason());
+                //status.setText(model.getVisitorstatus());
+
+                dialogPlus.show();
+
+                btnConfirmReject.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Map<String,Object> map = new HashMap<>();
+                        map.put("statusreason",sreason.getText().toString());
+                        map.put("visitorstatus",statusR);
+
+                        FirebaseDatabase.getInstance().getReference().child("Users").child("Visitors")
+                                .child(getRef(position).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(holder.sreason.getContext(), "Request Visit Rejected!", Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(Exception e) {
+                                        Toast.makeText(holder.sreason.getContext(), "Failed to Reject!", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
+                    }
+                });
+            }
+        });
 
 
+        holder.btnApprove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.img.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.approve_popup))
+                        .setExpanded(true, 480)
+                        .create();
+
+                //dialogPlus.show();
+
+                View v = dialogPlus.getHolderView();
+
+                String sreason = "Valid to visit";
+                String statusA = "Approved";
+
+                Button btnConfirmApprove = v.findViewById(R.id.btnConfirmApprove);
+
+                //sreason.setText(model.getStatusreason());
+                //status.setText(model.getVisitorstatus());
+
+                dialogPlus.show();
+
+                btnConfirmApprove.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Map<String,Object> map = new HashMap<>();
+                        map.put("statusreason",sreason);
+                        map.put("visitorstatus",statusA);
+
+                        FirebaseDatabase.getInstance().getReference().child("Users").child("Visitors")
+                                .child(getRef(position).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(holder.sreason.getContext(), "Request Visit Approved Successfully!", Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(Exception e) {
+                                        Toast.makeText(holder.sreason.getContext(), "Failed to Approve!", Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                });
+                    }
+                });
+            }
+        });
+
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.visitor_data_pending_officer,parent,false);
+        return new myViewHolder(view);
+    }
+
+    class myViewHolder extends RecyclerView.ViewHolder{
+        ImageView img;
+        TextView name, ic, phone, date, time, extime, status, ticket,reason, sreason;
+
+        Button btnReject, btnApprove;
+
+        public myViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            img = (ImageView)itemView.findViewById(R.id.img1);
+            name = (TextView)itemView.findViewById(R.id.visitor_name);
+            ic = (TextView)itemView.findViewById(R.id.visitor_ic);
+            phone = (TextView)itemView.findViewById(R.id.visitor_phone);
+            date = (TextView)itemView.findViewById(R.id.visit_date);
+            time = (TextView)itemView.findViewById(R.id.visit_time);
+            extime = (TextView)itemView.findViewById(R.id.visit_exit_time);
+            status = (TextView)itemView.findViewById(R.id.visitor_status);
+            ticket = (TextView)itemView.findViewById(R.id.visitor_ticket);
+            reason = (TextView)itemView.findViewById(R.id.visit_reason);
+            sreason = (TextView)itemView.findViewById(R.id.status_reason);
+
+            btnReject = (Button)itemView.findViewById(R.id.btnReject);
+            btnApprove = (Button)itemView.findViewById(R.id.btnApprove);
+
+        }
+    }
 }
